a4fe56fca644eaa240dd085d5491b247f1fd4b97
Theofanis94 <fkaramitsos@outlook.com>
2022-06-17 18:03:06 +0300

    commit1

--- index.ts
+++ index.ts
@@ -8,7 +8,7 @@ let map: google.maps.Map;
 
 function initMap(): void {
   map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
-    center: { lat: -34.397, lng: 150.644 },
+    center: { lat: 37.9838, lng: 23.7275 },
     zoom: 8,
   });
 }
